1b7874df367089c45496c86318a3fa6ee1356a9a
Arubaruba <shadowstep7@gmail.com>
2015-11-23 10:43:13 +0200

    complete example 2

--- LabMidterm/src/Ex02/StaticDemo.java
+++ LabMidterm/src/Ex02/StaticDemo.java
@@ -7,4 +7,13 @@ public class StaticDemo {
     static {
         stat2 = "b";
     }
+
+    static void printStatic() {
+        System.out.println("stat1: " + StaticDemo.stat1);
+        System.out.println("stat2: " + StaticDemo.stat2);
+    }
+
+    public static void main(String args[]) {
+        printStatic();
+    }
 }
